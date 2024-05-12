7a8e8a20163bf1ec51af262567ae5668abdf3b8d
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-05-13 00:10:58 +0200

    commit 2

--- proyecto1/src/app/persona/persona.component.ts
+++ proyecto1/src/app/persona/persona.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-persona',
-  standalone: true,
-  imports: [],
-  templateUrl: './persona.component.html',
-  styleUrl: './persona.component.css'
+	selector: 'app-persona',
+	standalone: true,
+	imports: [],
+	templateUrl: './persona.component.html',
+	styleUrl: './persona.component.css'
 })
 export class PersonaComponent {
-
+	nombre: string = 'Juan';
+	apellido: string = 'Lopez';
+	edad: number = 28;
+	
+	//&-----------------------------------------------------------------------
+	/* 
+	& Obtener un elemento HTML de id #entrada y 
+	& especificar que lo que devuelva eso se 
+	& convierta en un input al que se le obtendra el value
+	inputEntrada(): string{
+		return (document.getElementById("entrada") as HTMLInputElement).value;
+	} 
+	*/
+	//& Metodo y variable privada
+	/*
+	private edad: number = 28;
+	getEdad(): number{
+		return this.edad;
+	} 
+	*/
 }
--- proyecto1/src/app/personas/personas.component.ts
+++ proyecto1/src/app/personas/personas.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { PersonaComponent } from '../persona/persona.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-personas',
-  standalone: true,
-  imports: [PersonaComponent],
-  templateUrl: './personas.component.html',
-  styleUrl: './personas.component.css'
+	selector: 'app-personas',
+	standalone: true,
+	imports: [PersonaComponent, FormsModule],
+	templateUrl: './personas.component.html',
+	styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent {
-
+	deshabilitar: boolean = false;
+	mensaje: string = 'no se ha agregar gente';
+	titulon: string = 'inge mamalo';
+	agregarPersona() {
+		this.mensaje = "persona agregada";
+	}
+/* 	modificarTitulo(event: Event){
+		this.titulo = (<HTMLInputElement>event.target).value;
+	} */
 }
+
+//& styleUrl es para solo 1 css, styleUrls sirve para varios css
+//& styles y template es para escribir poco codigo css y html inline o usando backtick
